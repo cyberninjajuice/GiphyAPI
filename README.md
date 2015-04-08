028caaec194579f1ac2edcf16d0336854a340a6d
Raz <razgoldin@gmail.com>
2015-04-08 10:48:20 -0400

    made some progress with Search as an object which will give you search history

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # GiphyAPI
 A web App allowing users to submit queries to the Giphy API
+##Charlie & The Giphy Factory
+Charlie please see the linked proposal for your Giphy. This document will explain all the tools and features available to you by the time production is complete.
+## Who's it for?
+The Giphy Factory is for anyone who needs to see how their giphys are relating to their clientelle in the marketplace.
+
+##Deliverables
+This GitHub Repository containing the html source for the entire website
+
+##Usage
+The client may follow the instructions on the Web Application to Fill out the search query they would like to follow and then on the same page will be displayed certain Images from Giphy.
+
+##Further Progress
+For additional funding: The client will be able to pin Pictures from certain search terms to have them easily displayable to clientelle as sort of a Testimony to how great the product is.
+
+The links to the bookmarked Giphys will be stored in a local server. They will then be accessed by the client when they follow the path /saved. The links to the images will open up as a grid of pictures and he will have a convinient portfolio.
--- server.js
+++ server.js
@@ -1,1 +1,68 @@
-server.js
+//server.js
+var ejs = require("ejs");
+var express = require("express");
+
+var app = express();
+app.set("view_engine", "ejs");
+
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+var override = require("method-override");
+app.use(override("_method"));
+
+
+var counter = 0;
+
+function Search(query) {
+  this.id = counter;
+  this.query = query;
+
+  function this.access(this.query) {
+    var searched = this.query.toString().trim().split(" ");
+    searched.join("+");
+    var this.urlSearch = ejs.render("http://api.giphy.com/v1/gifs/search?q=<%-searched%>api_key=dc6zaTOxFJmzC", {
+      searched: searched
+    });
+    request(this.urlSearch, function(req, res) {
+      console.log(body);
+    });
+  };
+  this.image=this.access(this.query);
+  counter++;
+};
+var sample = new Search("puppies");
+
+var searches = {
+  0: sample,
+};
+app.get("/", function(req, res) {
+  res.redirect("/search");
+});
+app.get("/search", function(req, res) {
+  res.render("search.ejs");
+});
+app.post("/search", function(req, res) {
+  var newSearch = new Search(req.body.query);
+  searches[newSearch.id] = newSearch;
+  res.redirect("/show");
+});
+
+app.get("/search", function(req, res) {
+  request("http://ron-swanson-quotes.herokuapp.com/quotes", function(err, response, body) {
+    console.log(body);
+    var quote = JSON.parse(body).quote;
+    var data = ejs.render("show.ejs", {
+      quote: quote
+    });
+    res.send(data);
+  });
+});
+
+var port = 3000;
+
+app.listen(port, function() {
+  console.log("Server is listening on Port: " + port);
+});
